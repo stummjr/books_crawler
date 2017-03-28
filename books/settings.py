8dd389b9978011ff7ba30608eafd490ca38d3c60
Valdir Stumm Junior <stummjr@gmail.com>
2017-03-28 10:31:52 -0300

    remove dependencies

--- books/settings.py
+++ books/settings.py
@@ -6,19 +6,3 @@ SPIDER_MODULES = ['books.spiders']
 NEWSPIDER_MODULE = 'books.spiders'
 
 ROBOTSTXT_OBEY = True
-HTTPCACHE_ENABLED = True
-
-SPIDER_MIDDLEWARES = {
-    'scrapy_deltafetch.DeltaFetch': 100,
-    'scrapy_magicfields.MagicFieldsMiddleware': 200,
-}
-
-DELTAFETCH_ENABLED = True
-
-MAGICFIELDS_ENABLED = True
-MAGIC_FIELDS = {
-    "timestamp": "$time",
-    "spider": "$spider:name",
-    "url": "scraped from $response:url",
-    "domain": "$response:url,r'https?://([\w\.]+)/']",
-}
